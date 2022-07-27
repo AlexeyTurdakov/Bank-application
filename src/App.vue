bd64658aef79091099ffd2e2152d486cc48e93e3
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-07-27 17:45:44 +0400

    Layouts system
    +AuthLayout
    +MainLayout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <router-view />
+  <component :is="layout + '-layout'" v-if="layout"></component>
 </template>
 
-<style>
+<script>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import MainLayout from "./layout/MainLayout.vue";
+import AuthLayout from "./layout/AuthLayout.vue";
 
+export default {
+  setup() { 
+    const route = useRoute();
+
+    return { layout: computed(() => route.meta.layout) };
+  },
+
+  components: {
+    MainLayout,
+    AuthLayout,
+  },
+};
+</script>
+
+<style>
 </style>
---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <the-navbar></the-navbar>
+  <div class="container with-nav">
+    <div class="card">main</div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "../components/TheNavbar.vue";
+
+export default {
+  components: {
+    TheNavbar,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
